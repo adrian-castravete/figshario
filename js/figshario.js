b758bda15b7827b75beec309e38af7a4064d3f4c
Adrian Castravete <adrian@figshare.com>
2016-09-15 12:48:34 +0300

    Remove old source code

--- js/figshario.js
+++ js/figshario.js
@@ -32,12 +32,7 @@
       let c = this.canvas;
       let g = this.context;
 
-      let gd = g.createLinearGradient(0, 0, 0, c.height);
-      gd.addColorStop(0.00, "#888899");
-      gd.addColorStop(0.75, "#aaccff");
-      gd.addColorStop(1.00, "#9999bb");
-
-      g.fillStyle = gd;
+      g.fillStyle = "#000000";
       g.fillRect(0, 0, c.width, c.height);
     }
 
